d9af5b1d756c730ada3242f8725971ae133af512
kacharmich <103746021+kacharmich@users.noreply.github.com>
2023-04-10 00:12:09 +0300

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,43 @@
+﻿// Функция для изменения цвета фона текущей вкладки на черный
+function changeBackgroundColor() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            { code: 'document.body.style.backgroundColor = "#FFE4B5";' }
+        );
+    });
+}
+
+// Функция для добавления кнопки во всплывающее окно
+function addPopupButton() {
+    let button = document.createElement("button");
+    button.innerHTML = "Изменить цвет сайта на черный";
+    button.onclick = changeBackgroundColor;
+    document.body.appendChild(button);
+}
+
+
+// Функция для изменения цвета фона текущей вкладки на черный
+function changeBackgroundColor() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            { code: 'document.body.style.backgroundColor = "green";' }
+        );
+    });
+}
+// Функция для добавления кнопки во всплывающее окно
+function addPopupButton() {
+    let button = document.createElement("button");
+    button.innerHTML = "Изменить цвет сайта на зелёный";
+    button.onclick = changeBackgroundColor;
+    document.body.appendChild(button);
+}
+
+// Функция, которая будет вызвана при загрузке popup окна
+function onPopupLoad() {
+    addPopupButton();
+}
+
+// Добавляем обработчик события загрузки popup окна
+window.addEventListener("load", onPopupLoad);
